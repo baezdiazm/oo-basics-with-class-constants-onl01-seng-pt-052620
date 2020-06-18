6bb06c9bf898c7169cfbbc84a3bc56886118dbbd
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-18 22:48:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,10 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  def genre=(genre)
+    @genre = genre
+  end
 
   GENRES = []
 
